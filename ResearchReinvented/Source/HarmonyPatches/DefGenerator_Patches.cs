b1e5646235dd3129af7bbe4794214cbb37f2ca4b
PeteTimesSix <Pete1001@seznam.cz>
2022-10-11 14:09:58 +0200

    implemented DefGenerator

---
+++ ResearchReinvented/Source/HarmonyPatches/DefGenerator_Patches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using PeteTimesSix.ResearchReinvented.DefGenerators;
+using PeteTimesSix.ResearchReinvented.Defs;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeteTimesSix.ResearchReinvented.HarmonyPatches
+{
+    [HarmonyPatch(typeof(DefGenerator), nameof(DefGenerator.GenerateImpliedDefs_PreResolve))]
+    public static class DefGenerator_PreResolve_Patches
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            foreach(var alternateDef in AlternateResearchSubjectDefGenerator.AncientAlternateDefs()) 
+            {
+                DefGenerator.AddImpliedDef(alternateDef);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(DefGenerator), nameof(DefGenerator.GenerateImpliedDefs_PostResolve))]
+    public static class DefGenerator_PostResolve_Patches
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+
+        }
+    }
+}
