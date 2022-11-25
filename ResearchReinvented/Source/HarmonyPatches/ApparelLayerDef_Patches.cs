8ec85780a5f4f527a25895cbae746b597eb2e7fa
PeteTimesSix <Pete1001@seznam.cz>
2022-11-25 23:49:56 +0100

    visible kits

---
+++ ResearchReinvented/Source/HarmonyPatches/ApparelLayerDef_Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using PeteTimesSix.ResearchReinvented.DefOfs;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace PeteTimesSix.ResearchReinvented.HarmonyPatches
+{
+    [HarmonyPatch(typeof(ApparelLayerDef), nameof(ApparelLayerDef.IsUtilityLayer), MethodType.Getter)]
+    public static class ApparelLayerDef_Patches
+    {
+        [HarmonyPostfix]
+        public static bool ApparelLayerDef_IsUtilityLayer_Postfix(bool __result, ApparelLayerDef __instance) 
+        {
+            if (__result == true)
+                return __result;
+            else
+            {
+                return __instance == ApparelLayerDefOf_Custom.Satchel;
+            }
+        }
+    }
+}
