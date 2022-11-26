07808304f6af709faf14f36cbc021fd58751fcec
PeteTimesSix <Pete1001@seznam.cz>
2022-11-26 20:15:00 +0100

    added debug actions for debugging

--- ResearchReinvented/Source/Rimworld/DebugMenuEntries.cs
+++ ResearchReinvented/Source/Rimworld/DebugMenuEntries.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HarmonyLib;
+using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,7 @@ namespace PeteTimesSix.ResearchReinvented.Rimworld
     [StaticConstructorOnStartup]
     public static partial class DebugMenuEntries
     {
-        private const string CATEGORY = "Research roundabout";
+        private const string CATEGORY = "Research Reinvented";
 
         [DebugAction(category = CATEGORY, actionType = DebugActionType.Action)]
         static void ToggleDebugPrintouts() 
@@ -18,6 +20,31 @@ namespace PeteTimesSix.ResearchReinvented.Rimworld
             ResearchReinventedMod.Settings.debugPrintouts = !ResearchReinventedMod.Settings.debugPrintouts;
             string state = ResearchReinventedMod.Settings.debugPrintouts ? "on" : "off";
             Log.Message($"Toggled debug printouts {state}");
+        }
+
+        [DebugAction(category = CATEGORY, actionType = DebugActionType.Action)]
+        static void ResetResearchProgress()
+        {
+            Find.ResearchManager.ResetAllProgress();
+        }
+
+        [DebugAction(category = CATEGORY, actionType = DebugActionType.Action)]
+        static void RemoveAllResearch() 
+        {
+            var researchManager = Find.ResearchManager;
+            researchManager.ResetAllProgress();
+
+            var progressDict = AccessTools.Field(typeof(ResearchManager), "progress").GetValue(researchManager) as Dictionary<ResearchProjectDef, float>;
+
+            if (progressDict != null)
+            {
+                progressDict.Clear();
+                foreach (var allDef in DefDatabase<ResearchProjectDef>.AllDefs) 
+                {
+                    progressDict.Add(allDef, 0); 
+                }
+            }
+            researchManager.ReapplyAllMods();
         }
     }
 }
